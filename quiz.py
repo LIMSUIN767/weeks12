a0352779e9049e5207a77dc2f60d267e6f7cb642
SUIN LIM <lsi12312@handong.ac.kr>
2025-05-22 10:22:47 +0900

    1-4 하는 중

--- quiz.py
+++ quiz.py
@@ -12,8 +12,22 @@ df_subset.head()
 
 # [1-3] 결측치 확인 및 제거
 missing_counts_original = df_subset.isnull().sum()
-print(missing_counts_original)
+#print(missing_counts_original)
 df_cleaned = df_subset.dropna()
 
-missing_counts_cleaned = df_cleaned.isnull().sum()
-print(missing_counts_cleaned)
+#missing_counts_cleaned = df_cleaned.isnull().sum()
+#print(missing_counts_cleaned)
+
+# [1-4] 자료형 변환: 문자형 → 날짜형, 실수형 등
+# 날짜 컬럼: 문자열 → datetime
+df_cleaned['date'] = pd.to_datetime(df_cleaned['date'], errors='coerce')
+
+# 미세먼지 및 초미세먼지: 숫자형(float)
+df_cleaned['pm10'] = df_cleaned['pm10'].astype(float)
+df_cleaned['pm25'] = df_cleaned['pm25'].astype(float)
+
+# 자료형 확인
+df_cleaned.dtypes
+df_cleaned
+df_cleaned['date'].unique()
+print(df_cleaned['date'].isna().sum()
