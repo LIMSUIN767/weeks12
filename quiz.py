560ab8d2d3b2bf2c4818bdbdeb3a3ea91d69cca2
SUIN LIM <lsi12312@handong.ac.kr>
2025-05-22 10:11:24 +0900

    추가

--- quiz.py
+++ quiz.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('서울대기오염_2019.csv', encoding='utf-8')  # 또는 encoding='cp949' 필요할 수 있음
-print(df)
+#[1-1] 데이터프레임으로 불러오기
+df = pd.read_csv('201906.csv', encoding='utf-8')  # 또는 encoding='cp949' 필요할 수 있음
+
+# [1-2] 분석변수만 추출 및 컬럼명 변경: date, district, pm10, pm25
+df_subset = df[['날짜', '측정소명', '미세먼지', '초미세먼지']].copy()
+df_subset.columns = ['date', 'district', 'pm10', 'pm25']
+
+print(df_subset.head())
